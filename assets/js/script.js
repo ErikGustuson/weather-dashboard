322b81daab9c88f78bded7566071e1da1b9f97c7
Erik Gustuson <gusgus1919@gmail.com>
2021-10-02 18:11:51 -0700

    add starting pseudo code for js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// TODO: when searching for a city it will update the current and future conditions for that city
+// when entering city it should save to local store
+// TODO: when city is searched it will add it to the search history
+// one page load it should populate previously searched citys on list with a max of 4
+// when clicking on search button it should know to pull info from input box
+// TODO: current weather conditions should include -city name, the date, an icon of the weather, the temp, the humidity, the wind speed and the UV index
+// TODO: when viewing the uv index a color should indicate the condition as favorable, moderater or sever
+// TODO: 5 day forecast should display date, icon of the weather, the temp and humidity
+// TODO: when clicking on city in the search history the page should populate that citys current and future conditions
