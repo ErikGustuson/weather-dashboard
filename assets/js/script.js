8bdf43083b5262f80d2c4346a80afaed0af0834a
Erik Gustuson <gusgus1919@gmail.com>
2021-10-03 17:43:11 -0700

    correct issue with ajax and lon and lat var set previous city to show correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-// run function to display cits at page load
-init();
 // first i need to capture users input
 // i will need to target button and have it assing inner text as a string and assign it a var to use
 var searchedCity = document.getElementById("city-name");
 var searchButton = document.getElementById("search-city");
 var currentCity;
+var allCities = JSON.parse(localStorage.getItem("saved-city")) || [];
 
+// when clicking on search button it should know to pull info from input box
 searchButton.addEventListener("click", function (event) {
   event.preventDefault();
   // alert('ayyyyy you clicked me')
@@ -15,18 +15,25 @@ searchButton.addEventListener("click", function (event) {
 });
 
 // save input to local storage
+// when city is searched it will add it to the search history
 function saveCity() {
-  var savedCity = {
-    city: searchedCity.value,
-  };
-  localStorage.setItem("saved-city", JSON.stringify(savedCity));
+  allCities.unshift(searchedCity.value)
   showCity();
+  localStorage.setItem("saved-city", JSON.stringify(allCities));
 }
 
 // pull from storage
 function showCity() {
-  var lastCity = JSON.parse(localStorage.getItem("saved-city"));
-  document.getElementById("first-city").innerHTML = lastCity.city;
+  console.log(allCities)
+    if (allCities.length > 4) {
+      allCities.pop();
+    };
+  document.getElementById("first-city").innerHTML = allCities[0];
+  document.getElementById("second-city").innerHTML = allCities[1];
+  document.getElementById("third-city").innerHTML = allCities[2];
+  document.getElementById("fourth-city").innerHTML = allCities[3];
+  // if allCities.length is greater than 4, pop 
+  
 }
 
 // function ran at top when page loads
@@ -53,14 +60,12 @@ function getLonLat() {
     if (response.length == 0) {
       alert("Please enter a valid city name.");
     } else {
-      for (let i = 0; i < response.length; i++) {
-        console.log(response[i].lon);
-        console.log(response[i].lat);
-        lon = response[i].lon;
-        lat = response[i].lat;
-        saveCity();
-        getWeatherInfo()
-      }
+      console.log(response[0].lon);
+      console.log(response[0].lat);
+      lon = response[0].lon;
+      lat = response[0].lat;
+      saveCity();
+      getWeatherInfo(lat, lon);
     }
   });
 }
@@ -72,16 +77,16 @@ function getLonLat() {
 //     console.log(data);
 //   });
 
-// TODO: when searching for a city it will update the current and future conditions for that city
-// use lat and lon to get weather information to update page
-var weatherUrl =
-  "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-  lat +
-  "&lon=" +
-  lon +
-  "&exclude=minutely,hourly,alerts&units=imperial&appid=bcf0f3e083d40c7832b737bfb3c1e368";
 
-function getWeatherInfo() {
+// use lat and lon to get weather information to update page
+
+function getWeatherInfo(lat, lon) {
+  var weatherUrl =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&exclude=minutely,hourly,alerts&units=imperial&appid=bcf0f3e083d40c7832b737bfb3c1e368";
   $.ajax({
     url: weatherUrl,
     method: "GET",
@@ -90,10 +95,15 @@ function getWeatherInfo() {
   });
 }
 
-// TODO: when city is searched it will add it to the search history
-// one page load it should populate previously searched citys on list with a max of 4
-// when clicking on search button it should know to pull info from input box
+// TODO: when searching for a city it will update the current and future conditions for that city
+
+
 // TODO: current weather conditions should include -city name, the date, an icon of the weather, the temp, the humidity, the wind speed and the UV index
 // TODO: when viewing the uv index a color should indicate the condition as favorable, moderater or sever
 // TODO: 5 day forecast should display date, icon of the weather, the temp and humidity
 // TODO: when clicking on city in the search history the page should populate that citys current and future conditions
+
+
+// one page load it should populate previously searched citys on list with a max of 4
+// run function to display cits at page load
+init();
